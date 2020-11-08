4332e1845396f856d598395c5c3892dfabfe2315
Yau <ending1221@gmail.com>
2020-11-08 17:30:52 +0800

    [新增] outer寬度調整

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const $outer = document.querySelector('.outer');
+const bodyWidth = document.body.clientWidth;
+let scale = 1;
+
+if (bodyWidth < 550) scale = 0.9;
+if (bodyWidth < 500) scale = 0.7;
+if (bodyWidth < 400) scale = 0.6;
+
+$outer.style.transform = 'scale('+scale+')';
